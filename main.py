c379b90932a9339e3d2f44666b5270fd03140499
Brandon Luffman <brandonluff10@gmail.com>
2023-05-25 17:55:44 -0400

    rrequests

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 import spacy
 from pydantic import BaseModel
 
@@ -7,14 +8,14 @@ from pydantic import BaseModel
 
 app = FastAPI()
 nlp = spacy.load('./output/model-last')
-origins = ["*"]
+origins = ["*","http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    gallow_origins=origins,
 )
 class TextInput(BaseModel):
     text: str
