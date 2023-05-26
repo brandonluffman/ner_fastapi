b6ae456f2f4e0a77f8600a686a3f16981fa1e4cd
Brandon Luffman <brandonluff10@gmail.com>
2023-05-26 18:16:13 -0400

    new

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 from io import BytesIO, StringIO
 import tempfile
 
+
 # Initialize FastAPI
 app = FastAPI()
 
@@ -28,38 +29,53 @@ class Entity(BaseModel):
     end: int
     label: str
 
-# def load_model_from_s3(bucket_name, model_key, aws_access_key_id, aws_secret_access_key, region_name):
-#     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
-#     response = s3.get_object(Bucket=bucket_name, Key=model_key)
-#     model_bytes = response['Body'].read()
 
-#     with tempfile.NamedTemporaryFile() as temp_file:
-#         temp_file.write(model_bytes)
-#         temp_file.flush()
-#         model = spacy.load(temp_file.name)
-    
-#     return model
-
-# # Set up the AWS credentials
-# aws_access_key_id = 'AKIA6QR4X5D5UUHWU264'
-# aws_secret_access_key = 'dC//cBLCMRreENi6vfVAVS5EId15TP5pJwMsuqCF'
-# bucket_name = 'ner-model'
-# model_key = 'output/model-last'
-# region_name = 'us-east-1'  # Replace with your bucket's region
+# Set up the AWS credentials
+aws_access_key_id = 'AKIA6QR4X5D5UUHWU264'
+aws_secret_access_key = 'dC//cBLCMRreENi6vfVAVS5EId15TP5pJwMsuqCF'
+bucket_name = 'ner-model'
+model_key = 'model.tar.gz'
+region_name = 'us-east-1'  # Replace with your bucket's region
 
 
-# # Load the spaCy model from the S3 bucket
-# nlp = load_model_from_s3(bucket_name, model_key, aws_access_key_id, aws_secret_access_key, region_name)
+def load_model_from_s3(bucket_name, model_key):
+    # Create a session using your AWS credentials
+    session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    # Create an S3 client using the session
+    s3_client = session.client('s3')
+
+    # Download the model file from S3
+    try:
+        response = s3_client.get_object(Bucket=bucket_name, Key=model_key)
+        model_data = response['Body'].read()
+    except:
+        return None  # Return None or handle the error as per your requirement
+
+    # Load the model using spaCy from the in-memory buffer
+    model_buffer = BytesIO(model_data)
+    nlp = spacy.load(model_buffer)
+
+    return nlp
+
+
+class TextInput(BaseModel):
+    text: str
+
 
 # Define the API route
 @app.post("/model")
 async def ner_model(text_input: TextInput):
     text = text_input.text
-    # doc = nlp(text)
 
-    # entities = []
-    # for entity in doc.ents:
-    #     entities.append(Entity(text=entity.text, start=entity.start_char, end=entity.end_char, label=entity.label_))
+    nlp = load_model_from_s3(bucket_name, model_key)
 
-    # return entities, text
-    return text
+    if nlp is None:
+        return {'message': 'Failed to load model'}
+
+    # Perform any further operations with the loaded model
+    # For example, you can process the provided text using the model
+    doc = nlp(text)
+    entities = [(ent.text, ent.label_) for ent in doc.ents]
+
+    return {'message': 'Model loaded successfully', 'entities': entities}
--- test.py
+++ test.py
@@ -1,31 +1,31 @@
-import boto3
-from botocore.exceptions import ClientError
+# import boto3
+# from botocore.exceptions import ClientError
 
-def check_key_exists(bucket_name, key, aws_access_key_id, aws_secret_access_key, region_name):
-    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
-    try:
-        s3.head_object(Bucket=bucket_name, Key=key)
-        return True
-    except ClientError as e:
-        if e.response['Error']['Code'] == '404':
-            return False
-        else:
-            # Handle other exceptions as needed
-            raise
+# def check_key_exists(bucket_name, key, aws_access_key_id, aws_secret_access_key, region_name):
+#     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+#     try:
+#         s3.head_object(Bucket=bucket_name, Key=key)
+#         return True
+#     except ClientError as e:
+#         if e.response['Error']['Code'] == '404':
+#             return False
+#         else:
+#             # Handle other exceptions as needed
+#             raise
 
-aws_access_key_id = 'AKIA6QR4X5D5UUHWU264'
-aws_secret_access_key = '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'
-bucket_name = 'ner-model'
-model_key = 'output/model-last'
-region_name = 'us-east-1'
+# aws_access_key_id = 'AKIA6QR4X5D5UUHWU264'
+# aws_secret_access_key = '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'
+# bucket_name = 'ner-model'
+# model_key = 'output/model-last'
+# region_name = 'us-east-1'
 
-s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+# s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
 
-exists = check_key_exists(bucket_name, model_key, aws_access_key_id, aws_secret_access_key, region_name)
-if exists:
-    print(f"The key '{model_key}' exists within the bucket '{bucket_name}'.")
-else:
-    print(f"The key '{model_key}' does not exist within the bucket '{bucket_name}'.")
+# exists = check_key_exists(bucket_name, model_key, aws_access_key_id, aws_secret_access_key, region_name)
+# if exists:
+#     print(f"The key '{model_key}' exists within the bucket '{bucket_name}'.")
+# else:
+#     print(f"The key '{model_key}' does not exist within the bucket '{bucket_name}'.")
 
-all_objects = s3.list_objects(Bucket = bucket_name) 
-print(all_objects)
+# all_objects = s3.list_objects(Bucket = bucket_name) 
+# print(all_objects)
