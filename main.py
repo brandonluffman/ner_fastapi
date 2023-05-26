4693172ed21d5322139d5f1ea084ea84c66a4dd6
Brandon Luffman <brandonluff10@gmail.com>
2023-05-26 09:55:21 -0400

    s3

--- main.py
+++ main.py
@@ -3,11 +3,32 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 import spacy
 from pydantic import BaseModel
-
+import boto3
+import os
 
 
 app = FastAPI()
-# nlp = spacy.load('./output/model-last')
+
+def download_model_from_s3():
+    s3 = boto3.client('s3')
+    bucket_name = 'ner-model'
+    model_key = 'output/model-last'
+    local_directory = './output'
+
+    # Create the local directory if it doe,sn't exist
+    os.makedirs(local_directory, exist_ok=True)
+
+    # Download the model files from S3
+    for obj in s3.list_objects_v2(Bucket=bucket_name, Prefix=model_key)['Contents']:
+        key = obj['Key']
+        file_name = os.path.join(local_directory, key.replace(model_key, '').lstrip('/'))
+        os.makedirs(os.path.dirname(file_name), exist_ok=True)
+        s3.download_file(bucket_name, key, file_name)
+
+
+s3 = boto3.client('s3', aws_access_key_id='AKIA6QR4X5D5UUHWU264', aws_secret_access_key='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')
+nlp = spacy.load('./output/model-last')
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,12 +49,12 @@ class Entity(BaseModel):
 @app.post("/model")
 async def ner_model(text_input: TextInput):
     text = text_input.text
-    # doc = nlp(text)
+    doc = nlp(text)
 
-    # entities = []
-    # for entity in doc.ents:
-    #     entities.append(Entity(text=entity.text, start=entity.start_char, end=entity.end_char, label=entity.label_))
+    entities = []
+    for entity in doc.ents:
+        entities.append(Entity(text=entity.text, start=entity.start_char, end=entity.end_char, label=entity.label_))
 
-    # return entities, text
+    return entities, text
     
-    return text
+    # return text
